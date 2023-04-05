8ebbfc90fe02b2460a88735c53045378b5093949
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-04-05 20:42:56 +0200

    User Model v1

---
+++ src/models/User.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// To encrypt password
+var bcrypt = require('bcryptjs');
+var SALT_WORK_FACTOR = 10;
+// Modelado de datos para users
+var UserSchema = new mongoose.Schema({
+    username: {type: String, required: true, index: {unique: true}}, 
+    name: {type: String, required: true}, 
+    surnames: {type: String, required: true}, 
+    password: {type: String, required: true}, 
+    idnumber: {type: String, required: true, index: {unique: true}}, 
+    email: {type: String, required: true, index: {unique: true}}, 
+    phone: {type: String, required: true}, 
+    address: {type: String, required: true}, 
+    birthday: {type: Date, required: true}, 
+    register_date: {type: Date, default: Date.now, required: true}, 
+    avatar: {type: String, default:'xxx'}, 
+    status: {type: String, enum: ['inactive', 'active'], default: 'active'}, 
+    role: {type: String, enum: ['client', 'employee'], default: 'client'}, 
+    // —------------- Si el ‘role’ es ‘client’: —---------------------------
+    pets: [{type: mongoose.Schema.Types.ObjectId, ref: Pet, default: null}], 
+    // —------------- Si el ‘role’ es ‘employee’: —---------------------------
+    start_date: {type: Date, default: Date.now}, 
+    end_date: {type: Date}, 
+    studies: [{type: String}], 
+/*     position: {{
+        title: {type: String},
+        salary: {type: Number},
+        reportsTo: {type: mongoose.Schema.Types.ObjectId},
+        }
+    } */
+})
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+    // evaluates if it has hashed password or is modified
+    if (!user.isModified('password')) return next();
+    // creates salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        // use salt when hashing password
+        bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+            // overwrites the password
+            user.password = hash;
+            next();
+        });
+    });
+    });
+    // Method to compare password and log user if matches
+    UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+        bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+            if (err) return cb(err);
+            cb(null, isMatch);
+        });
+    };
+    module.exports = mongoose.model('User', UserSchema);
