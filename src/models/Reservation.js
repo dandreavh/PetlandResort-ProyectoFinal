d43fd3222b5e0c3b4eb2d844b20a3654f14e3856
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-04-07 15:10:42 +0200

    Reservation model

---
+++ src/models/Reservation.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// Refered models
+const User = require('../models/User.js');
+const Pet = require('../models/Pet.js');
+// Reservations model
+const ReservationSchema = new mongoose.Schema({
+    checkin: {type: Date, required: true},
+	checkout: {type: Date, required: true},
+	timestamp: {type: Date, default: Date.now, required: true},
+	room: {
+		type: {type: String, enum: ['suite', 'pack suite', 'deluxe suite', 'deluxe pack suite'], default: 'suite', required: true},
+		assigned: {type: String},
+		details: {type: String}		
+    },
+	client: {type: mongoose.Schema.Types.ObjectId, required: true}, 
+	pets: [{type: mongoose.Schema.Types.ObjectId, required: true}], 
+    petsitter: [{type: mongoose.Schema.Types.ObjectId}], 
+    observations: {type: String}, 
+    cares: [{
+        description: {type: String},
+        pet: {type: mongoose.Schema.Types.ObjectId},
+        observations: {type: String},
+        scheduled: {type: Date},
+        giver: {type: mongoose.Schema.Types.ObjectId},
+    }]
+})
+
+module.exports = mongoose.model('Reservation', ReservationSchema);
