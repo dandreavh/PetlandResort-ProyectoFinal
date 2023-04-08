dad1d216dac50257633124a6abcbe6a5c1dff454
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-04-08 10:41:35 +0200

    Login and register v1

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('./pages/index', { title: 'Express' });
+  res.render('./pages/index');
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('./pages/register');
 });
 
 module.exports = router;
