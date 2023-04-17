c6b5d013fb502a92d8b643f6aa4645b0ecd06e8d
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-04-17 11:32:18 +0200

    Override and Flash

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride = require('method-override');
 
 // ODM configuration - database
 const mongoose = require('mongoose');
@@ -25,12 +26,25 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// dependencies call
+// static files placement
+app.use(express.static(path.join(__dirname, '/public')));
+
+// dependencies call (middlewares)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(flash());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/public')));
+
+// global variables for flash messages and user
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
 
 // using routes
 app.use('/', indexRouter);
