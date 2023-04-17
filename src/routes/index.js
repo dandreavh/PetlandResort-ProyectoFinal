450566617cf3ce3cf5f38a2ef38d266c34b7d241
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-04-17 12:08:21 +0200

    Render pages basic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,12 +8,24 @@ router.get('/', function(req, res, next) {
 
 /* GET register page. */
 router.get('/register', function(req, res, next) {
+  // evaluate if user logged is admin, manager or not
   res.render('./pages/registerAdmin');
 });
 
-/* GET home client */
-router.get('/homeClient', function(req, res, next) {
-  res.render('./pages/homeClient');
+/* GET reservarions page */
+router.get('/reservations', function(req, res, next) {
+  // check  if user is logged 
+  res.render('./pages/reservations');
+});
+
+/* GET rooms page */
+router.get('/rooms', function(req, res, next) {
+  res.render('./pages/rooms');
+});
+
+/* GET about us page */
+router.get('/aboutus', function(req, res, next) {
+  res.render('./pages/aboutus');
 });
 
 module.exports = router;
