25cc1806e197e49d49e457efbc0473f89bb55356
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-06-07 12:50:30 +0200

    Express-validator

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
+const { check, validationResult } = require('express-validator');
 
 // ODM configuration - database
 const mongoose = require('mongoose');
