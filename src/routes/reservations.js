2d6f8acc76831d99afa2879786d334034b3767ac
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-06-07 11:44:45 +0200

    add and edit reservation

--- src/routes/reservations.js
+++ src/routes/reservations.js
@@ -17,18 +17,46 @@ ______________________________________________________________________
 */
 // GET all the users order by registration date
 router.get('/listReservations', async (req, res) =>{
-    const reservations = await Reservation.find();
+    const reservations = await Reservation.find().sort('-checkin');;
     res.send(reservations);
 });
 
+// GET to render to addReservation (post) with all the pets related to the client
 router.get('/addReservation', isAuthenticated, async (req, res) => {
     const userLogged = req.user;
     const petsList = await Pet.find({'caregiver': userLogged.username});
     res.render('./pages/addReservation', {petsList});
 })
 
+router.get('/editReservation/:id', isAuthenticated, async (req, res) => {
+    console.log("In editReservation");
+    const reservation = await Reservation.findById(req.params.id);
+    const userLogged = req.user;
+    const petsList = await Pet.find({'caregiver': userLogged.username});
+    if (!reservation) {
+        req.flash('error_msg', 'Ha habido un error al encontrar la reserva');
+    } else{
+        const currentDate = new Date();
+        const checkin = new Date(reservation.checkin);
+        if (checkin > currentDate) {
+            res.render('./pages/editReservation', {reservation, petsList});
+        } else{
+            req.flash('error_msg', 'Esta reserva no se puede modificar');
+            res.redirect('/home');
+        }
+    }
+})
+
+
+
+router.put('/editReservation/:id', isAuthenticated, async (req, res) => {
+    await Reservation.findByIdAndUpdate(req.params.id, req.body);
+    req.flash('success_msg', 'Reserva modificada con éxito'); 
+    res.redirect('/home');
+})
+
 // POST to create a new reservation (General)
-router.post('/addReservation', isAuthenticated, async function(req, res) {
+router.post('/addReservation', isAuthenticated, async (req, res) => {
     console.log("In addReservation");
     if(isAuthenticated){
         const userLogged = req.user;
@@ -38,6 +66,24 @@ router.post('/addReservation', isAuthenticated, async function(req, res) {
         req.flash('error_msg', 'Ha habido un error al realizar la reserva');
     }
     res.redirect('../home');
+});
+
+// DELETE to remove a reservation if checkin is in a future moment
+router.delete('/removeReservation/:id', isAuthenticated, async (req, res) => {
+    console.log("In removeReservation");
+    const reservation = await Reservation.findById(req.params.id);
+    if (!reservation) {
+        req.flash('error_msg', 'Ha habido un error al encontrar la reserva');
+    } else{
+        const currentDate = new Date();
+        const checkin = new Date(reservation.checkin);
+        if (checkin > currentDate) {
+            await Reservation.findByIdAndDelete(req.params.id);
+            req.flash('success_msg', 'Reserva eliminada con éxito'); 
+        } else {
+            req.flash('error_msg', 'Esta reserva no se puede eliminar');
+        }
+    }
 });
 
 module.exports = router;
